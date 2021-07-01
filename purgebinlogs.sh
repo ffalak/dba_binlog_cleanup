5ada22e21be30cb97fb10b3fbace8bd14bf65870
Franjo Falak <72608309+ffalak@users.noreply.github.com>
2021-07-01 15:22:05 +0200

    Update purgebinlogs.sh

--- purgebinlogs.sh
+++ purgebinlogs.sh
@@ -11,9 +11,9 @@ done
 
 if [[ -n $sati ]]; then
         last_n_hours="$(date -d ${sati}' hours ago' '+%Y-%m-%d %T')"
-        mysql --execute="PURGE BINARY LOGS BEFORE '"${last_n_hours}"';"
+        mysql --execute="PURGE BINARY LOGS BEFORE '${last_n_hours}';"
     elif [[ -n $binlog_name ]]; then
-        mysql --execute="PURGE BINARY LOGS TO '"${binlog_name}"';"
+        mysql --execute="PURGE BINARY LOGS TO '${binlog_name}';"
     else
         echo "ERROR: No argument"
      exit 1
