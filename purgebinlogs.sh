32c3fa13d4e3339af3ea6221f8672eec4b31f986
Franjo Falak <72608309+ffalak@users.noreply.github.com>
2021-07-01 12:45:59 +0200

    purgebinlogs.sh
    Skripta za purganje binlogova

---
+++ purgebinlogs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Bash script for purging binlogs
+
+while getopts t:n: flag
+do
+    case "${flag}" in
+        t) sati=${OPTARG};;
+        n) binlog_name=${OPTARG};;
+    esac
+done
+
+last_n_hours="$(date -d ${sati}' hours ago' '+%Y-%m-%d %T')"
+
+if [[ -n $sati ]]; then
+        mysql --execute="PURGE BINARY LOGS BEFORE ${last_n_hours};"
+    elif [[ -n $binlog_name ]]; then
+      mysql --execute="PURGE BINARY LOGS TO ${binlog_name};"
+    else
+        echo "ERROR: No argument" 
+     exit 1
+fi
