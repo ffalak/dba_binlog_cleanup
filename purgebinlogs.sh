1bceb974d8e75863f736515597b5471d366dacf5
Franjo Falak <72608309+ffalak@users.noreply.github.com>
2021-07-02 16:10:43 +0200

    Update purgebinlogs.sh
    Changed variable name from Croatian to English

--- purgebinlogs.sh
+++ purgebinlogs.sh
@@ -4,13 +4,13 @@
 while getopts t:n: flag
 do
     case ${flag} in
-        t) sati="${OPTARG}";;
+        t) hours="${OPTARG}";;
         n) binlog_name="${OPTARG}";;
     esac
 done
 
 if [[ -n $sati ]]; then
-        last_n_hours="$(date -d ${sati}' hours ago' '+%Y-%m-%d %T')"
+        last_n_hours="$(date -d ${hours}' hours ago' '+%Y-%m-%d %T')"
         mysql --execute="PURGE BINARY LOGS BEFORE '${last_n_hours}';"
     elif [[ -n $binlog_name ]]; then
         mysql --execute="PURGE BINARY LOGS TO '${binlog_name}';"
